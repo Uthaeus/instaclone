3de2ddbd56f0fef11e1d7437536ab387868f4232
Uthaeus <romanlavery@gmail.com>
2018-07-13 10:44:44 -0600

    Created js.erb files for create and destroy for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,12 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id 
 
     if @comment.save
-      flash[:success] = "Comment created"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
-      flash[:alert] = "Check the comment form"
+      flash[:alert] = "Check the comment form, something went wrong"
       render root_path
     end
   end
@@ -17,10 +19,14 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @post.comments.find(params[:id])
 
-    @comment.destroy
-    flash[:success] = "Comment deleted"
+    if @comment.user_id == current_user.id 
+      @comment.delete
 
-    redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
   end
 
 
