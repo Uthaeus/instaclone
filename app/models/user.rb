9493025b16d522a84953d5cfbf2651845a7cd3d3
Uthaeus <romanlavery@gmail.com>
2018-07-12 21:53:50 -0600

    Created partial for index and show, Added comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  
+
   validates :user_name, presence: true, length: { minimum: 4, maximum: 20 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
