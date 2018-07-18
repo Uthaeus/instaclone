e98b201658f5768a8b70736b22105a33f6f7c2d2
Uthaeus <romanlavery@gmail.com>
2018-07-18 13:20:48 -0600

    Fixed js issue with loading more comments

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -2,11 +2,10 @@ var Append = {};
 Append.open = false;
 function ClickableCommentsLink(){
   $('.more-comments').click( function() {
-    $(this).on('ajax:success', function(event, data, status,xhr) {
-      event.preventDefault();
+    $(this).on('ajax:success', function(event) {
       var postId = $(this).data("post-id");
-      $("#comments_" + postId).html(data);
-      $("#comments-paginator-" + postId).html("<a id='more-comments' data-post-id=" + postId + "data-type='html' data-remote='true' href='/posts/" + postId + "/comments>show morecomments</a>");
+      $("#comments_" + postId).html(event.detail[2].responseText);
+      $("#comments-paginator-" + postId).html("show more comments");
       Append.open = true;
       Append.comment = true; 
       Append.link = false;  
