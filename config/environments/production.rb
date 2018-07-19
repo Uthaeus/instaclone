b9c3ef58dce4ce6d0a17bf775a40d2810f79b2ef
Uthaeus <romanlavery@gmail.com>
2018-07-19 09:07:03 -0600

    Adjustments to paperclip functionality with aws, now working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    url: ':s3_domain_url',
+    path: ':class/:attachment/:id_partition/:style/:filename',
     s3_region: ENV['AWS_REGION'],
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
